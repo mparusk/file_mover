972fe5f2c13c7fab797ea9993270642b4afcff5f
Maria Parusk <mariaparusk@live.com>
2019-08-28 00:02:49 +0100

    Initial pseudocode for the mover

---
+++ filemover.sh
@@ -0,0 +1,8 @@
+#!/bin/bash	
+
+#create new directory in the parent folder
+#for each directory, move all the contents to the new directory
+#print $(totaldirs) - $(deleted dirs)  out of $(totaldirs) done
+#delete directory (or even better, move to trash?)
+#in the end print all files (should probably be 144)
+
